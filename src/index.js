56ad9e3b68951f4f2e99ecae9958b01124cb5139
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-05-09 16:53:40 +0200

    trying

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const API_URL = 'https://restcountries.com/v3.1/all';
+const inputEl = document.getElementById('search-box');
+
+// fetchCountries(name);
+
+fetch(API_URL)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response.status);
+    }
+  })
+  .then(data => {
+    console.log(data[5].name.common);
+  })
+  .catch(error => {
+    console.log('zjebało się');
+  });
